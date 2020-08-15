8a69bfa1f5fb5bc0c4cc1d2255ca58a39b47fb8c
Wirgiliusz <kamx98@gmail.com>
2020-08-15 14:10:49 +0200

    Implemented merge sort

--- CH1_Sequential-sorting-algorithms/merge_sort.c
+++ CH1_Sequential-sorting-algorithms/merge_sort.c
@@ -3,12 +3,70 @@
 
 #define ARRAY_LEN 10
 
+void merge(int array[], int l, int m, int r) {
+    int L_len = m - l + 1;
+    int R_len = r - m;
+    int array_L[L_len];
+    int array_R[R_len];
+    for (int i = 0; i < L_len; ++i) {
+        array_L[i] = array[l+i];
+    }
+    for (int i = 0; i < R_len; ++i) {
+        array_R[i] = array[m+1+i];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+    while (i < L_len && j < R_len) {
+        if (array_L[i] <= array_R[j]) {
+            array[k] = array_L[i];
+            ++i;
+        } else {
+            array[k] = array_R[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < L_len) {
+        array[k] = array_L[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < R_len) {
+        array[k] = array_R[j];
+        ++j;
+        ++k;
+    }
+}
+
+void merge_sort(int array[], int l, int r) {
+    if (r > l) {
+        int m = (l+r)/2;
+        merge_sort(array, l, m);
+        merge_sort(array, m+1, r);
+
+        merge(array, l, m, r);
+    }
+}
+
+
 int main(void) {
-    int array[ARRAY_LEN] = {0, 8, 5, 3, 4, 9, 2, 1, 6};
+    int array[ARRAY_LEN] = {0, 8, 5, 3, 4, 9, 2, 1, 6, 7};
 
     printf("Unsorted array:\n");
     for (int i=0; i<ARRAY_LEN; ++i) {
         i==(ARRAY_LEN-1) ? printf("%d\n", array[i]) : printf("%d ", array[i]);
+    }
+
+    printf("Sorting...\n");
+    merge_sort(array, 0, ARRAY_LEN-1);
+
+    printf("Sorted array:\n");
+    for (int i=0; i<ARRAY_LEN; ++i) {
+        i==ARRAY_LEN ? printf("\n") : printf("%d ", array[i]);
     }
 
     return EXIT_SUCCESS;
